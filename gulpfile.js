0f90b4c332a5d90c2685c7f8a5555907821a6ac4
geomaas <geoff.maas@gmail.com>
2016-07-11 13:55:09 -0400

    building file directory

---
+++ gulpfile.js
@@ -0,0 +1,44 @@
+'use strict'
+
+let gulp = require('gulp');
+let sass = require('gulp-sass');
+let browserify = require('gulp-browserify');
+
+gulp.task('default', ['html', 'css', 'js','watch']);
+
+gulp.task('html', function () {
+  gulp.src('./templates/*.html').pipe(gulp.dest('./public/templates'));
+
+    return gulp.src('./index.html')
+        .pipe(gulp.dest('./public'));
+});
+
+gulp.task('css', function () {
+    return gulp.src('./styles.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('./public'));
+});
+
+
+gulp.task('js', function () {
+  gulp.src('./js/controllers/*.js').pipe(gulp.dest('./public/js/controllers'));
+  gulp.src('./js/services/*.js').pipe(gulp.dest('./public/js/services'));
+
+
+    return gulp.src('./js/app.js')
+        .pipe(browserify())
+        .pipe(gulp.dest('./public/js'))
+});
+
+gulp.task('watch', function () {
+    gulp.watch('./*.js', ['js']);
+    gulp.watch('./*/*.js', ['js']);
+    gulp.watch('./*/*/*.js', ['js']);
+    gulp.watch('./*.scss', ['css']);
+    gulp.watch('./index.html', ['html']);
+    gulp.watch('./templates/*.html', ['html']);
+    gulp.watch('./js/controllers/*.js,'['js']);
+    gulp.watch('./js/services/*.js,'['js']);
+
+
+});
